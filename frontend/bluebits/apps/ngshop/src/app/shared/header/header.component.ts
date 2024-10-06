15ee1ad3da1335cf10a75e420916c7c7b3ac71bc
LuchulumancoGDev <131538691+LuchulumancoGDev@users.noreply.github.com>
2024-10-06 19:56:57 +0200

    ng shop begin

--- frontend/bluebits/apps/ngshop/src/app/shared/header/header.component.ts
+++ frontend/bluebits/apps/ngshop/src/app/shared/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavComponent } from "../nav/nav.component";
 
 @Component({
   selector: 'ngshop-header',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+
 })
 export class HeaderComponent {
 
---
+++ frontend/bluebits/apps/ngshop/src/app/shared/nav/nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngshop-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav.component.html',
+  styles: ``
+})
+export class NavComponent {
+
+}
