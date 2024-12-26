1dc6365b779178d70b9b8bd2ba467749330712b8
LuchulumancoGDev <131538691+LuchulumancoGDev@users.noreply.github.com>
2024-12-27 00:10:13 +0200

    updates

--- frontend/bluebits/libs/products/src/lib/pages/product-page/product-page.component.ts
+++ frontend/bluebits/libs/products/src/lib/pages/product-page/product-page.component.ts
@@ -9,23 +9,25 @@ import { CommonModule } from '@angular/common';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonModule } from 'primeng/button';
 import { GalleryComponent } from '@bluebits/ui';
+import { CartItem, CartService } from '@bluebits/orders';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'lib-products-product-page',
   standalone: true,
-  imports: [RatingModule,FormsModule, CommonModule, InputNumberModule, ButtonModule, GalleryComponent],
+  imports: [RatingModule,FormsModule, CommonModule,ToastModule, InputNumberModule, ButtonModule, GalleryComponent],
+  providers: [MessageService],
   templateUrl: './product-page.component.html',
   styles: ``
 })
 export class ProductPageComponent implements OnInit, OnDestroy {
-addProductToCart() {
-throw new Error('Method not implemented.');
-}
-  quantity?: number ;
+
+  quantity?: number =1 ;
 
   product: Product | undefined;
   endSubs$: Subject<any> = new Subject();
-  constructor(private productService:ProductsService, private route: ActivatedRoute) {
+  constructor(private productService:ProductsService, private messageService: MessageService, private route: ActivatedRoute, private cartService:CartService) {
 
   }
 
@@ -37,15 +39,24 @@ throw new Error('Method not implemented.');
     })
   }
 
-  // addProductToCart() {
-
-  // }
+  addProductToCart() {
+    const cartItem: CartItem ={
+      productId: this.product?.id,
+      quantity: this.quantity
+    }
+    this.cartService.setCartItem(cartItem);
+    this.show();
+  }
   private _getProducts(id: string) {
     this.productService.getProduct(id).pipe(takeUntil(this.endSubs$)).subscribe(response => {
       this.product = response;
     });
 
   }
+
+   show() {
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: `Successfully added ${this.product?.name} item to cart` });
+    }
 
    ngOnDestroy(): void {
     this.endSubs$.next(this.product),
